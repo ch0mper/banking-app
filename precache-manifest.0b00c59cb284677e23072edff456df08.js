360669c3223f8b18613d1edb652604ca073730d4
Hannah Chang <35381777+ch0mper@users.noreply.github.com>
2020-07-22 12:42:19 -0500

    Updates

---
+++ precache-manifest.0b00c59cb284677e23072edff456df08.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "112ae9dbb04833fed764d3f7622652fc",
+    "url": "/banking-app/index.html"
+  },
+  {
+    "revision": "6bc786f9d5faa8a5bdaa",
+    "url": "/banking-app/static/css/main.5523ad1d.chunk.css"
+  },
+  {
+    "revision": "7b22c2cfb5bb05e5e8a4",
+    "url": "/banking-app/static/js/2.d9c6eb83.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/banking-app/static/js/2.d9c6eb83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bc786f9d5faa8a5bdaa",
+    "url": "/banking-app/static/js/main.8255d926.chunk.js"
+  },
+  {
+    "revision": "1248e5e22a445e0ff068",
+    "url": "/banking-app/static/js/runtime-main.89a93517.js"
+  },
+  {
+    "revision": "0b9fe056a7d681a29a51937f97032ef0",
+    "url": "/banking-app/static/media/Calendar.0b9fe056.svg"
+  },
+  {
+    "revision": "7c0fb25ecfe23429f2c80c27db486f5c",
+    "url": "/banking-app/static/media/Chart.7c0fb25e.svg"
+  },
+  {
+    "revision": "54a819eb62dd29c41bab0bcdbec5dc56",
+    "url": "/banking-app/static/media/Home.54a819eb.svg"
+  },
+  {
+    "revision": "907a048ad35d6c6435ad9cac8ebaa6ef",
+    "url": "/banking-app/static/media/Search.907a048a.svg"
+  },
+  {
+    "revision": "f4c92e6835715e27f07952222d8ffdb3",
+    "url": "/banking-app/static/media/Settings.f4c92e68.svg"
+  }
+]);
